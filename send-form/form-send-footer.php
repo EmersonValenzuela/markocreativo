7fbfa36326125c54734fa4a47f2f291f7c155c51
noelmoriano <mariano260996@gmail.com>
2020-06-22 00:45:34 -0500

    UPDATE: send form php 8

--- send-form/form-send-footer.php
+++ send-form/form-send-footer.php
@@ -60,7 +60,7 @@ if(isset($_POST['btn-send-message-fo'])){
     $emailValue = "";
     $messageValue =  "";
     
-    header("location:../index.html");
+    header("location:http://markocreativo.com/");
 
         echo "<script>
                     if ( window.history.replaceState ) {
